b15ae0aea619efe2673712d505510ec4a20d9588
Jordan Hoover <15951744+JordanHoover@users.noreply.github.com>
2018-03-09 16:38:55 -0700

    code quality update

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,16 +5,15 @@ n= 20
 i = 3
 
 # multiples of j print fizz buzz
-
 j = 5
 
 # range operation in python is inclusive on bottom end
 # and exclusive on top/second input
 # so to go from 1 to 100, need range n+1
 for x in range(1,n+1):
-    if x%j==0:
+    if x % j ==0:
         print("fizz buzz")
-    elif x%i==0:
+    elif x % i==0:
         print("fizz")
     else:
         print(x)
@@ -25,7 +24,7 @@ for x in range(1,n+1):
 # argument must be the bigger multiple
 
 def fizz_buzz(n, fizz, fizz_buzz):
-    for x in range(1,n+1):
+    for x in range(1, n+1):
         if x % fizz_buzz == 0: print("fizz buzz")
         elif x % fizz == 0: print("fizz")
         else: print(x)
